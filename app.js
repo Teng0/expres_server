2c1b6a4425126949da583f071ba724229e39deca
Tengo <tengosmail@gmail.com>
2019-04-02 23:26:40 +0400

    Second commit

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var nunjucks = require('nunjucks');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/product');
 
 var app = express();
+
 
 
 // ისეთი ფუნქციის შექმნა რომელიც პარამეტს იღებს და midlware ს აბრუნებს
@@ -19,6 +22,13 @@ var m = function(param){
       next();
   }
 }
+//Express Session ის შემოტანა
+app.use(session({
+  secret: 'randomKey',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
 
 //midware ს დამატება
@@ -44,13 +54,27 @@ var m = function(param){
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//ესენი PUG ის სამუშაოთ
+//app.set('views', path.join(__dirname, 'views'));
+
+//app.set('view engine', 'pug');
+
+
+//ეს nunjucks ის სამუშაოთ
+app.set('views2', path.join(__dirname, 'views2'));
+nunjucks.configure('views2', {
+  autoescape: true,
+  express: app
+});
+
+
+app.set('view engine', 'nunjucks');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/product.js
+++ routes/product.js
@@ -1,4 +1,5 @@
 let express = require("express");
+var session = require('express-session')
 
 let productRouter = express.Router();
 
@@ -19,16 +20,23 @@ var protected = function(key){
 
 }
 
+
+
+
 //productRouter.get("/:id", protected('123'),(req,res)=>{    // ლინკის ვალიდაციის ამბავი Protected ფუნქციით
 productRouter.get("/:id",(req,res)=>{
 
     let product={
         id:123,
         name:"test",
-        price:10
+        price:10,
+        images:["image1","image2"]
     }
+//cookies ს შექმნა
+    res.cookie('name', 'tobi');
     //res.send("params = "+req.params.id);
-    res.render('product',product)
+   // res.render('product',product) ეს pug ის სამუშაოთ
+    res.render('product.html',product)
   });
 
 productRouter.get("/",function(req,res){
